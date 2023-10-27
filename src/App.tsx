33468bf7afc0b56736ff03276c657d8d29a7b650
zielinp <pzielinska.zielona@gmail.com>
2023-10-27 19:30:09 +0200

    Config update

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,169 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
 
 import React from 'react';
 
-const App = () => {
-  return (
-    <div className="bg-blue-500 p-4">
-      <h1 className="text-2xl font-bold text-white">Witaj w mojej aplikacji z Tailwind CSS!</h1>
-      <p className="text-white">To jest przykładowy tekst.</p>
-    </div>
-  );
-};
 
-export default App;
+
+
+const people = [
+  {
+    name: 'Calvin Hawkins',
+    email: 'calvin.hawkins@example.com',
+    image:
+      'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+  {
+    name: 'Kristen Ramos',
+    email: 'kristen.ramos@example.com',
+    image:
+      'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+  {
+    name: 'Ted Fox',
+    email: 'ted.fox@example.com',
+    image:
+      'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+]
+
+export default function App() {
+  // return (
+  //   <ul className="divide-y divide-gray-200">
+  //     {people.map((person) => (
+  //       <li key={person.email} className="py-4 flex">
+  //         <img className="h-10 w-10 rounded-full" src={person.image} alt="" />
+  //         <div className="ml-3">
+  //           <p className="text-sm font-medium text-gray-900">{person.name}</p>
+  //           <p className="text-sm text-gray-500">{person.email}</p>
+  //         </div>
+  //       </li>
+  //     ))}
+  //   </ul>
+  // )
+
+  return (
+    // <div>
+    //   <label htmlFor="price" className="block text-sm font-medium leading-6 text-gray-900">
+    //     Price
+    //   </label>
+    //   <div className="relative mt-2 rounded-md shadow-sm">
+    //     <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+    //       <span className="text-gray-500 sm:text-sm">$</span>
+    //     </div>
+    //     <input
+    //       type="text"
+    //       name="price"
+    //       id="price"
+    //       className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+    //       placeholder="0.00"
+    //     />
+    //     <div className="absolute inset-y-0 right-0 flex items-center">
+    //       <label htmlFor="currency" className="sr-only">
+    //         Currency
+    //       </label>
+    //       <select
+    //         id="currency"
+    //         name="currency"
+    //         className="h-full rounded-md border-0 bg-transparent py-0 pl-2 pr-7 text-gray-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
+    //       >
+    //         <option>USD</option>
+    //         <option>CAD</option>
+    //         <option>EUR</option>
+    //       </select>
+    //     </div>
+    //   </div>
+    // </div>
+
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            className="mx-auto h-10 w-auto"
+            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            alt="Your Company"
+          />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Sign in to your account
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" action="#" method="POST">
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                  Password
+                </label>
+                <div className="text-sm">
+                  <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                    Forgot password?
+                  </a>
+                </div>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+
+          <p className="mt-10 text-center text-sm text-gray-500">
+            Not a member?{' '}
+            <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+              Start a 14 day free trial
+            </a>
+          </p>
+        </div>
+      </div>
+  )
+
+}
+
+// const App = () => {
+//   return (
+//     <div className="bg-blue-500 p-4">
+//       <h1 className="text-2xl font-bold text-red">Witaj w mojej aplikacji z Tailwind CSS!</h1>
+//       <p className="text-white">To jest przykładowy tekst.</p>
+//     </div>
+//   );
+
+// };
+// export default App;
+
 
 
 // function App() {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
-/** @type {import('tailwindcss').Config} */
+import autoprefixer from 'autoprefixer'; // Zamiast require('autoprefixer')
+
+
 export default {
-  content: [],
+  content: ['./src/**/*.{html,js,ts,jsx,tsx}',],
   theme: {
     extend: {},
   },
   plugins: [
-    require('autoprefixer'), // Dodaj tę linijkę
+    autoprefixer, 
   ],
-}
-
-
+};
